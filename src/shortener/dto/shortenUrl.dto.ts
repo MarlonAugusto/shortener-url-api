bfe5aca86dd6d107a2c1354474147e8a94b0a01e
MarlonAugusto <l.marlonaugusto@gmail.com>
2025-02-24 22:56:33 -0300

    feat: criação dos DTOs

---
+++ src/shortener/dto/shortenUrl.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class ShortenUrlDTO {
+    @IsNotEmpty()
+    @IsString()
+    originalUrl: string;
+
+    @IsOptional()
+    userId: number;
+}
