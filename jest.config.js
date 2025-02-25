662da8f53814ea4903ff521d6abff59c5c2d4dd6
MarlonAugusto <l.marlonaugusto@gmail.com>
2025-02-25 14:59:15 -0300

    feat: ajuste nas porcentagens dos testes unitários

--- jest.config.js
+++ jest.config.js
@@ -6,18 +6,22 @@ module.exports = {
   coverageReporters: ["json", "lcov", "text", "html"],
   coverageThreshold: {
     global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
   collectCoverageFrom: [
     "src/**/*.ts",
     "!src/main.ts",
-    "!src/**/*.module.ts", 
-    "!src/**/models/*", 
-    "!src/**/dto/*", 
+    "!src/**/*.module.ts",
+    "!src/**/models/*",
+    "!src/**/dto/*",
     "!src/database/**/*",
+    "!src/**/types/*",
   ],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
 };
